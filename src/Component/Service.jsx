545f9cab05a7bc90ed9485b27c4637cc58668956
Vishal Saluja <salujavishal370@gmail.com>
2024-11-28 19:34:05 +0530

    First Commit

--- src/Component/Service.jsx
+++ src/Component/Service.jsx
@@ -19,67 +19,64 @@ const Service = () => {
   };
 
 return (
- <div className="Service">
- <div className="Service_Upper_Heading">
- <h1 className="Service10">Start your AI journey</h1>
+<div className="Service">
+<div className="Service_Upper_Heading">
+<h1 className="Service10">Start your AI journey</h1>
 <h3 className="Service15">Quickstart with our highly rated courses in AI, ML and data science</h3>
 </div>
-    <div className="Service3">
-      <div className="Service4">
-        <img src="https://analytics-vidhya.vercel.app/images/data_analytics.jpg" alt="random-image" className="image_Service"/>
-        <h2 className="Service_Heading">Data Analytics</h2>
-        <h6 className="Service_para">Unlock the power of data with our comprehensive<br/>
-        Data Analytics designed to equip you<br/>
-        with essential skills for extracting insights<br/>
-         and making informed decisions.</h6>
-         <div className="Service6">
-          <h4 className="Service5">Python</h4>
-          <h4 className="Service5">Pandas</h4>
-          <h4 className="Service8">Data Visualization</h4>
-        </div>
-        <div>
-         <button className="Enroll"  onClick={handleButtonClick}  >Enroll Now</button> 
-        </div>
-        </div>  
+<div className="Service3">
+<div className="Service4">
+<img src="https://analytics-vidhya.vercel.app/images/data_analytics.jpg" alt="random-image" className="image_Service"/>
+<h2 className="Service_Heading">Data Analytics</h2>
+<h6 className="Service_para">Unlock the power of data with our comprehensive<br/>
+Data Analytics designed to equip you<br/>
+with essential skills for extracting insights<br/>
+and making informed decisions.</h6>
+<div className="Service6">
+<h4 className="Service5">Python</h4>
+<h4 className="Service5">Pandas</h4>
+<h4 className="Service8">Data Visualization</h4>
+</div>
+<div>
+<button className="Enroll"  onClick={handleButtonClick}  >Enroll Now</button> 
+</div>
+</div>  
        
-      <div className="Service4">
+<div className="Service4">
+<img src="https://analytics-vidhya.vercel.app/images/Python.jpg" alt="random-image" className="image_Service"/>
+<h2 className="Service_Heading">Python for Data Science</h2>
+<h6 className="Service_para">Unlock the power of data with our comprehensive<br/>
+Data Analytics designed to equip you<br/>
+with essential skills for extracting insights<br/>
+and making informed decisions.</h6>
+<div className="Service6">
+<h4 className="Service5">Python</h4>
+<h4 className="Service5">Pandas</h4>
+<h4 className="Service8">Data Visualization</h4>
+</div>
+<div>
+<button className="Enroll"  onClick={handleButton2Click}  >Enroll Now</button> 
+ </div>
 
-      <img src="https://analytics-vidhya.vercel.app/images/Python.jpg" alt="random-image" className="image_Service"/>
-        <h2 className="Service_Heading">Python for Data Science</h2>
-        <h6 className="Service_para">Unlock the power of data with our comprehensive<br/>
-        Data Analytics designed to equip you<br/>
-        with essential skills for extracting insights<br/>
-         and making informed decisions.</h6>
-         <div className="Service6">
-          <h4 className="Service5">Python</h4>
-          <h4 className="Service5">Pandas</h4>
-          <h4 className="Service8">Data Visualization</h4>
-        </div>
-        <div>
-         <button className="Enroll"  onClick={handleButton2Click}  >Enroll Now</button> 
-        </div>
+ </div>
+  <div className="Service4">
+  <img src="https://analytics-vidhya.vercel.app/images/SQL.jpg" alt="random-image" className="image_Service"/>
+  <h2 className="Service_Heading">Basics of SQL</h2>
+  <h6 className="Service_para">Master the fundamentals of SQL with our<br/>
+  comprehensive course designed to teach<br/>
+  you database querying, management, and manipulation
+  </h6>
+  <div className="Service6">
+  <h4 className="Service5">Mongodb</h4>
+  <h4 className="Service5">Express</h4>
+  <h4 className="Service8">React</h4>
+  </div>
+  <div>
+  <button className="Enroll"  onClick={handleButton3Click}  >Enroll Now</button> 
+  </div>
 
-      </div>
-      <div className="Service4">
-       <img src="https://analytics-vidhya.vercel.app/images/SQL.jpg" alt="random-image" className="image_Service"/>
-        <h2 className="Service_Heading">Basics of SQL</h2>
-        <h6 className="Service_para">Master the fundamentals of SQL with our<br/>
-          comprehensive course designed to teach<br/>
-          you database querying, management, and manipulation
-        </h6>
-         <div className="Service6">
-          <h4 className="Service5">Python</h4>
-          <h4 className="Service5">Pandas</h4>
-          <h4 className="Service8">Data Visualization</h4>
-        </div>
-        <div>
-         <button className="Enroll"  onClick={handleButton3Click}  >Enroll Now</button> 
-        </div>
-
-      </div>
-    </div>
-
-
+  </div>
+  </div>
  </div>
   )
 }
